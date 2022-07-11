57041703f9615016fc4a8993de412d5d77629d52
KamenSentai <cvt.alain@gmail.com>
2022-07-11 14:13:37 +0200

    feat(): update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  publicRuntimeConfig: {
+    collection: process.env.APP_COLLECTION,
+    iconNetwork: process.env.APP_ICON_NETWORK,
+    scoreAddress: process.env.APP_SCORE_ADDRESS,
+    logoHash: process.env.APP_LOGO_HASH,
+    unrevealedHash: process.env.APP_UNREVEALED_HASH,
+  },
+  app: {
+    head: {
+      title: process.env.APP_NAME,
+      titleTemplate: `%s - ${process.env.APP_NAME}`,
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
 })
