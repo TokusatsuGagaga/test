3659af229abfd6865ee9a1d84bcb623da83feec4
KamenSentai <cvt.alain@gmail.com>
2022-07-14 20:40:23 +0200

    feat(): update

--- stores/ledger.ts
+++ stores/ledger.ts
@@ -170,124 +170,16 @@ export const useLedgerStore = defineStore('ledger-store', () => {
   }
   const getLedgerAddresses = async (page: number): Promise<LedgerAddressesList> => {
     try {
-      console.log('ITEMS_PER_PAGE:', ITEMS_PER_PAGE)
       const transport = await TransportWebHID.create()
-      console.log('transport:', transport)
       const icx = new Icx(transport)
-      console.log('icx:', icx)
-
-      let iiiiiddddd
-
-      iiiiiddddd = 0
-      try {
-        console.log('start test')
-        const id = iiiiiddddd
-        console.log('id:', id)
-        const { address } = await icx.getAddress(`44'/4801368'/0'/0'/${id}'`)
-        console.log('address:', address)
-        const result = await iconService.getBalance(String(address)).execute()
-        console.log('result:', result)
-        const balance = IconService.IconConverter.toNumber(result) / 10 ** 18
-        console.log('balance:', balance)
-        console.log('end test')
-      } catch (error) {
-        console.error(`something went wrong with id ${iiiiiddddd}`)
-      }
-
-      iiiiiddddd = 1
-      try {
-        console.log('start test')
-        const id = iiiiiddddd
-        console.log('id:', id)
-        const { address } = await icx.getAddress(`44'/4801368'/0'/0'/${id}'`)
-        console.log('address:', address)
-        const result = await iconService.getBalance(String(address)).execute()
-        console.log('result:', result)
-        const balance = IconService.IconConverter.toNumber(result) / 10 ** 18
-        console.log('balance:', balance)
-        console.log('end test')
-      } catch (error) {
-        console.error(`something went wrong with id ${iiiiiddddd}`)
-      }
-
-      iiiiiddddd = 2
-      try {
-        console.log('start test')
-        const id = iiiiiddddd
-        console.log('id:', id)
-        const { address } = await icx.getAddress(`44'/4801368'/0'/0'/${id}'`)
-        console.log('address:', address)
-        const result = await iconService.getBalance(String(address)).execute()
-        console.log('result:', result)
-        const balance = IconService.IconConverter.toNumber(result) / 10 ** 18
-        console.log('balance:', balance)
-        console.log('end test')
-      } catch (error) {
-        console.error(`something went wrong with id ${iiiiiddddd}`)
-      }
-
-      iiiiiddddd = 3
-      try {
-        console.log('start test')
-        const id = iiiiiddddd
-        console.log('id:', id)
-        const { address } = await icx.getAddress(`44'/4801368'/0'/0'/${id}'`)
-        console.log('address:', address)
-        const result = await iconService.getBalance(String(address)).execute()
-        console.log('result:', result)
-        const balance = IconService.IconConverter.toNumber(result) / 10 ** 18
-        console.log('balance:', balance)
-        console.log('end test')
-      } catch (error) {
-        console.error(`something went wrong with id ${iiiiiddddd}`)
-      }
-
-      iiiiiddddd = 4
-      try {
-        console.log('start test')
-        const id = iiiiiddddd
-        console.log('id:', id)
-        const { address } = await icx.getAddress(`44'/4801368'/0'/0'/${id}'`)
-        console.log('address:', address)
-        const result = await iconService.getBalance(String(address)).execute()
-        console.log('result:', result)
-        const balance = IconService.IconConverter.toNumber(result) / 10 ** 18
-        console.log('balance:', balance)
-        console.log('end test')
-      } catch (error) {
-        console.error(`something went wrong with id ${iiiiiddddd}`)
-      }
-
-      console.log('start ledgerBook')
-      // const ledgerBook: LedgerAddressesList = await Promise.all([...new Array(ITEMS_PER_PAGE)].map(async (_, index) => {
-      //   const id = ITEMS_PER_PAGE * page + index
-      //   console.log('id:', id)
-      //   const { address } = await icx.getAddress(`44'/4801368'/0'/0'/${id}'`)
-      //   console.log('address:', address)
-      //   const result = await iconService.getBalance(String(address)).execute()
-      //   console.log('result:', result)
-      //   const balance = IconService.IconConverter.toNumber(result) / 10 ** 18
-      //   console.log('balance:', balance)
-      //   return {
-      //     id,
-      //     address: String(address),
-      //     path: `44'/4801368'/0'/0'/${id}'`,
-      //     balance,
-      //     isLoading: false,
-      //   } as LedgerAddressData
-      // }))
 
       const ledgerBook: LedgerAddressesList = []
       for (let index = (ITEMS_PER_PAGE * page); index < ((ITEMS_PER_PAGE * page) + ITEMS_PER_PAGE); index++) {
-        console.log('id:', index)
         // eslint-disable-next-line no-await-in-loop
         const { address } = await icx.getAddress(`44'/4801368'/0'/0'/${index}'`)
-        console.log('address:', address)
         // eslint-disable-next-line no-await-in-loop
         const result = await iconService.getBalance(String(address)).execute()
-        console.log('result:', result)
         const balance = IconService.IconConverter.toNumber(result) / 10 ** 18
-        console.log('balance:', balance)
         ledgerBook.push({
           id: index,
           address: String(address),
@@ -296,7 +188,6 @@ export const useLedgerStore = defineStore('ledger-store', () => {
           isLoading: false,
         })
       }
-      console.log('end ledgerBook')
 
       return ledgerBook
     } catch (error) {
